a017129b6e87f136fce7880e3b6f42f800821d4a
Dam-0 <damien.peters2000@gmail.com>
2023-12-11 18:57:22 +0800

    First varible json post body

--- main.go
+++ main.go
@@ -57,43 +57,39 @@ func main() {
 
 	for i := 0; i < len(data.Leak_nodes); i++ {
 		ip := data.Leak_nodes[i].Ip
-		resource_id := data.Leak_nodes[i].Resource_id
-		leaks_found := data.Leak_nodes[i].Leak_count
-		total_leaks := data.Leak_nodes[i].Leak_event_count
-		open_ports := data.Leak_nodes[i].Open_ports
+		//	resource_id := data.Leak_nodes[i].Resource_id
+		//	leaks_found := data.Leak_nodes[i].Leak_count
+		//	total_leaks := data.Leak_nodes[i].Leak_event_count
+		//	open_ports := data.Leak_nodes[i].Open_ports
+		//  first_seen := data.Leak_nodes[i].Creation_date
+		//  last_updated := data.Leak_nodes[i].Update_date
 
-		for x := 0; x < len(data.Leak_nodes[i].Events); x++ {
-			e_summary := data.Leak_nodes[i].Events[x].Summary
-			e_time := data.Leak_nodes[i].Events[x].Time
-			e_event_source := data.Leak_nodes[i].Events[x].Event_source
-			e_host_ip := data.Leak_nodes[i].Events[x].Ip
-			e_host_address := data.Leak_nodes[i].Events[x].Host
-			e_host_ports := data.Leak_nodes[i].Events[x].Port
-			e_protocol := data.Leak_nodes[i].Events[x].Protocol
-			e_root := data.Leak_nodes[i].Events[x].HTTP.Root
-			e_http_url := data.Leak_nodes[i].Events[x].HTTP.URL
-			e_organisation := data.Leak_nodes[i].Events[x].Network.Organisation
-
-		}
-
-		first_seen := data.Leak_nodes[i].Creation_date
-		last_updated := data.Leak_nodes[i].Update_date
+		//	for x := 0; x < len(data.Leak_nodes[i].Events); x++ {
+		//		e_summary := data.Leak_nodes[i].Events[x].Summary
+		//		e_time := data.Leak_nodes[i].Events[x].Time
+		//		e_event_source := data.Leak_nodes[i].Events[x].Event_source
+		//		e_host_ip := data.Leak_nodes[i].Events[x].Ip
+		//		e_host_address := data.Leak_nodes[i].Events[x].Host
+		//		e_host_ports := data.Leak_nodes[i].Events[x].Port
+		//		e_protocol := data.Leak_nodes[i].Events[x].Protocol
+		//		e_root := data.Leak_nodes[i].Events[x].HTTP.Root
+		//		e_http_url := data.Leak_nodes[i].Events[x].HTTP.URL
+		//		e_organisation := data.Leak_nodes[i].Events[x].Network.Organisation
+		//}
 
 		webhook_url := os.Getenv("webhook_key")
 
-		fmt.Println("HTTP JSON POST URL:", webhook_url)
+		var jsonData = []byte(fmt.Sprintf(`{
+			"username": "Leak Notification",
+			"embeds": [
+			  {
+				"title": "Test Title",
+				"description": "This is the IP: %v "
+			  }
+			]
+		  }`, ip))
 
-		values := map[string]string{
-			"username": "WebHook Test",
-			"content":  ip,
-		}
-
-		jsonData, error := json.Marshal(values)
-		if error != nil {
-			panic(error)
-		}
-
-		fmt.Printf("\nThis is the value: %s \n", ip)
+		fmt.Printf("\nThis is the value: %s \n", jsonData)
 
 		request, error := http.NewRequest("POST", webhook_url, bytes.NewBuffer(jsonData))
 		if error != nil {
@@ -113,5 +109,4 @@ func main() {
 		fmt.Println("response Body:", string(body))
 
 	}
-
 }
